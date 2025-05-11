49fb205f62cf13ae816cea751a1d0126261b2466
Hannah McKay <hannah.denise.mckay@gmail.com>
2025-05-11 20:44:27 +0100

    direct website to correct port

--- app.py
+++ app.py
@@ -205,4 +205,6 @@ def load_user(user_id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
