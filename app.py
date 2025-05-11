fc81cd53224f2f4e1bf20a83edfc56149420c8c7
Hannah McKay <hannah.denise.mckay@gmail.com>
2025-05-11 22:07:00 +0100

    redo proc

--- app.py
+++ app.py
@@ -215,7 +215,6 @@ def load_user(user_id):
     return User.get_by_id(user_id)
 
 
+# Only needed for local dev, not when using gunicorn
 if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run()
