b6cc8825b06d202ee6f14f4597f95a9cf145881a
Harrun Muhiden <harrunmuhiden@gmail.com>
2025-06-09 14:52:32 +0100

    Ethan's quiz commit

--- game_src/GCSE/GCSE7/main.py
+++ game_src/GCSE/GCSE7/main.py
@@ -95,8 +95,6 @@ def main():
                 if submit_button.collidepoint(event.pos):
                     task = loop.create_task(check_answers())
                     pending_tasks.append(task)
-
-window.parent.postMessage("level_complete_gcse7", "*")
         
         for i, (q, pos) in enumerate(zip(questions, question_positions)):
             text = FONT.render(q, True, FG_COLOR)
@@ -119,6 +117,8 @@ window.parent.postMessage("level_complete_gcse7", "*")
             pass
 
         clock.tick(60)
+        
+    window.parent.postMessage("level_complete_gcse7", "*")
 
     pygame.quit()
     sys.exit()
