984b22e54264527d1902caa38f52c2604e5650c0
EthanCross29 <125976886+EthanCross29@users.noreply.github.com>
2025-06-09 14:53:40 +0100

    Update main.py

--- game_src/GCSE/GCSE3/main.py
+++ game_src/GCSE/GCSE3/main.py
@@ -50,7 +50,6 @@ def handle_answer(answer):
     
     asyncio.create_task(check_answer_async(answer))
 
-window.parent.postMessage("level_complete_gcse3", "*")
 
 class Button:
     def __init__(self, x, y, text, callback):
@@ -107,6 +106,7 @@ async def main_loop():
         await asyncio.sleep(0)  
         clock.tick(60)
 
+    window.parent.postMessage("level_complete_gcse3", "*")
     pygame.quit()
     sys.exit()
 
